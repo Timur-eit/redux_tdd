b888b82916f426da6652f2dce2d0f0fc7f2f63d1
Evgeny <evtsyga@gmail.com>
2022-01-04 21:24:35 +0300

    add test case

--- src/double.test.ts
+++ src/double.test.ts
@@ -4,4 +4,5 @@ test('double 2 to equal 4', () => {
     expect(double(2)).toBe(4);
     expect(double(2)).toBeGreaterThanOrEqual(3);
     expect(double(2)).toBeLessThan(8);
+    expect(double(2)).not.toBeNull();
 })
