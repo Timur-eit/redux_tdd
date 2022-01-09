8616f890af149246325ec891c433d7b4ec703313
Evgeny <evtsyga@gmail.com>
2022-01-09 18:39:20 +0300

    implement dispatch

--- src/createStore.spec.ts
+++ src/createStore.spec.ts
@@ -1,38 +1,36 @@
 import { createStore } from './createStore';
 
 describe('createStore', () => {
-  it("should return object with subscribe, dispatch & getState methods", () => {
-    const store = createStore(x => x);
+	it("should return object with subscribe, dispatch & getState methods", () => {
+		const store = createStore(x => x);
     
-    expect(store).toHaveProperty("dispatch");
-    expect(store).toHaveProperty("getState");
-    expect(store).toHaveProperty("subscribe");
-  })
-  it ('default state should be initialState', () => {
-    const store = createStore((s) => s, 'qwerty');
-    const state = store.getState();
+    	expect(store).toHaveProperty("dispatch");
+    	expect(store).toHaveProperty("getState");
+    	expect(store).toHaveProperty("subscribe");
+  	})
+  	it ('default state should be initialState', () => {
+    	const store = createStore((s) => s, 'qwerty');
+    	const state = store.getState();
 
-    expect(state).toEqual('qwerty');
-  })
-  it('default state should be equal default reducer state in case there no second param', () => {
-    const store = createStore((x = "asdfgh") => x);
-    const state = store.getState();
+    	expect(state).toEqual('qwerty');
+  	})
+  	it('default state should be equal default reducer state in case there no second param', () => {
+    	const store = createStore((x = "asdfgh") => x);
+    	const state = store.getState();
 
-    expect(state).toEqual('asdfgh');
-  })
-  it ('after dispatch state should be changed', () => {
-      const reducer = (state: number | undefined, action : { type: string }) => {
-        if (action.type === 'increment') {
-            return state! + 1;
-        }
-        return state;
-      };
-      const store = createStore(reducer, 0);
-      store.dispatch({type: 'increment'});
-      const state = store.getState();
+    	expect(state).toEqual('asdfgh');
+	})
+  	it ('after dispatch state should be changed', () => {
+    	const reducer = (state: number | undefined, action: { type: string }) => {
+        	if (action.type === 'increment') {
+        		return state! + 1;
+        	}
+        	return state;
+    	};
+    	const store = createStore(reducer, 0);
+    	store.dispatch({type: 'increment'});
+    	const state = store.getState();
 
-      expect(state).toEqual(1);
-      
-  })
-  
+    	expect(state).toEqual(1);
+  	})
 })
--- src/createStore.ts
+++ src/createStore.ts
@@ -10,11 +10,14 @@ const store = createStore<number, { type: "a" | "b" }>(x => x);
 // store.dispatch({type: 1});
 
 export function createStore<S, A extends Action>(reducer: Reducer<S | undefined, A>, initialState?: S) {
-    const state = reducer(initialState, { type: "@@redux/INIT" } as A);
-    const dispatch = (action: A) => {
-        
-    };
+    let state = reducer(initialState, { type: "@@redux/INIT" } as A);
+
     const getState = () => state;
+    const dispatch = (action: A) => {
+        const currentState = getState();
+        state = reducer(getState(), action);
+    };
     const subscribe = () => { return };
-    return {dispatch, getState, subscribe };
+
+    return { dispatch, getState, subscribe };
 }
